740a73d650f8abe636dc35feffc8e0ca2dba63fc
Lei Deng <ldeng.sjtu@gmail.com>
2026-01-30 19:31:48 +0800

    add qwen3-32b script

---
+++ run-qwen3-32b.py
@@ -0,0 +1,34 @@
+import gc
+import torch
+
+from vllm import LLM, SamplingParams
+from vllm.distributed.parallel_state import (destroy_distributed_environment,
+                                             destroy_model_parallel)
+
+def clean_up():
+    destroy_model_parallel()
+    destroy_distributed_environment()
+    gc.collect()
+    torch.npu.empty_cache()
+
+prompts = [
+    "Hello, my name is",
+    "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.6, top_p=0.95, top_k=40)
+llm = LLM(model="/docker/models/Qwen3-32B",
+          tensor_parallel_size=8,
+          trust_remote_code=True,
+          distributed_executor_backend="mp",
+          max_model_len=5500,
+          max_num_batched_tokens=5500,
+          compilation_config={"cudagraph_mode": "FULL_DECODE_ONLY"})
+
+outputs = llm.generate(prompts, sampling_params)
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+del llm
+clean_up()
